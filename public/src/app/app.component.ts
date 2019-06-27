dafe8efd145046554bed8ca7026a7de270d7ee77
sushidools <sashadooley@gmail.com>
2019-06-27 10:33:11 -0600

    first commit

---
+++ public/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(title: Title) {
+    title.setTitle('Aleksandra Dooley');
+  }
+}
---
+++ server.js
@@ -0,0 +1,15 @@
+const parser = require('body-parser');
+const express = require('express');
+
+const app = express();
+const { PORT: port = 1999 } = process.env;
+
+require('./server/config/database');
+
+app.use(express.static(__dirname + '/public/dist/public'));
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+// app.use(require('./server/routes'));
+
+app.listen(port, () => console.log(`Express server listening on port ${port}`));
