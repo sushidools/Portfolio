79aa554a72a18de8ecc5bea64eb1d84bc9804101
sushidools <sashadooley@gmail.com>
2019-07-12 22:47:34 -0600

    back to design

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const parser = require('body-parser');
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const { PORT: port = 1999 } = process.env;
+const port = process.env.PORT || 5000;
 
 require('./server/config/database');
 
@@ -11,5 +12,8 @@ app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 
 // app.use(require('./server/routes'));
+app.all('*', (req, res, next) => {
+  res.sendFile(path.resolve('./public/dist/public/index.html'));
+});
 
 app.listen(port, () => console.log(`Express server listening on port ${port}`));
